2e242b38d2fd18069f38217be4a9ffb9b4531a94
Dickson25-dot <dicksonndereba25@gmail.com>
2025-03-08 00:00:47 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+// let writeCards= (["Guadalupe", "Ollie", "Aki"], "surprise")
+// for (let writeCard Of writeCards){
+    // return `Thank you, ${writecards}, for the wonderful surprise gift!`
+// }
+function writeCards(names=["Guadalupe", "Ollie", "Aki"],event= "surprise"){
+    return names.map(name =>  `Thank you, ${name}, for the wonderful ${event} gift!`);
+  }
+  function countDown(number= 10) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
